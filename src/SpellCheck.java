d55a07af4c614f17c9f22d944ed84fe931833a07
thinkie72 <tyler.hinkie@menloschool.org>
2024-09-26 23:29:02 -0700

    Work Log Entry 4

--- src/SpellCheck.java
+++ src/SpellCheck.java
@@ -20,9 +20,17 @@ public class SpellCheck {
      * @return String[] of all misspelled words in the order they appear in text. No duplicates.
      */
     public String[] checkWords(String[] text, String[] dictionary) {
-        Set<String> dict = new HashSet<>(Arrays.asList(dictionary));
+        // Hashed Way: fastest I've found
+        // Turns the dictionary into a Hashset
+        Set<String> dictionary_v2 = new HashSet<>(Arrays.asList(dictionary));
+        // Creates Hash Set to hold misspelled words, but as a linked one to maintain the order of the added words
         Set<String> misspelled = new LinkedHashSet<>();
-        for (String word : text) if (!dict.contains(word)) misspelled.add(word);
+        // Checks if the dictionary contains a given word for each word in the given text, adds to misspelled if not
+        for (String word : text) if (!dictionary_v2.contains(word)) misspelled.add(word);
+        // Returns the array version of misspelled
         return misspelled.toArray(new String[0]);
+
+        // Linear and Iterative Way coming next
+
     }
 }
